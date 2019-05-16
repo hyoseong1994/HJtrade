1d004650ff4b0e5c464de340912a8aa43237e7e9
hyoseong3291 <wjdgytjd3291@gmail.com>
2019-05-16 15:08:47 +0900

    메인

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,21 +1,22 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
+			Scene scene = new Scene(root);
+
+			primaryStage.setTitle("육류 유통관리 프로그램");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
